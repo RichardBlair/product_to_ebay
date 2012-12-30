a4c79c966190e3fceadff785bf69e8f372edebe7
Richard L. Blair <richard@blairingthoughts.com>
2012-12-29 22:25:02 -0500

    Move logic into forms

--- ebay_product/ebay_product/views.py
+++ ebay_product/ebay_product/views.py
@@ -1,49 +1,32 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.datastructures import MultiValueDictKeyError
-from django.core.urlresolvers import reverse
 from django.views.generic import View, TemplateView
-from django.conf import settings
 
-from shopify.api import Shoppy
-
-from .models import ApplicationCharge
+from ebay_product.forms import ActivateChargeForm, CreateChargeForm
 
 
 class InstallView(View):
     """
     View to handle creating a charge and activating a charge.
-
-    TODO:
-        Move the logic into a form.
     """
 
     def get(self, request, *args, **kwargs):
-        shop = Shoppy(access_token=request.user.access_token,
-                shop=request.user.myshopify_domain)
-
         try:
-            charge = ApplicationCharge.objects.get(shop=request.user,
-                    charge_id=request.GET['charge_id'])
-            shop.activateRecurringCharge(id=charge.charge_id)
-            charge.status = 1
-            charge.save()
-        except (ApplicationCharge.DoesNotExist, MultiValueDictKeyError):
-            charge = {
-                        'recurring_application_charge': {
-                                'name': 'eBay lister',
-                                'price': 4.99,
-                                'test': True,
-                                'return_url': '%s%s' % (settings.DOMAIN,
-                                    reverse('install'))
-                            }
-                    }
-            resp = shop.createRecurringCharge(data=charge)
+            form_data = {'charge_id': request.GET['charge_id']}
+            activate_form = ActivateChargeForm(request.user, data=form_data)
 
-            #This could raise a key error, and im not catching it on purpose.
-            #If a person is having a problem signing up I want it to be very loud.
-            ApplicationCharge.objects.create(shop=request.user,
-                    price=charge['recurring_application_charge']['price'],
-                    name=charge['recurring_application_charge']['name'],
-                    charge_id=resp['recurring_application_charge']['id']
-                    )
-            return HttpResponseRedirect(resp['recurring_application_charge']['confirmation_url'])
+            if activate_form.is_valid():
+                activate_form.save()
+                return HttpResponse("Installed")
+        except (MultiValueDictKeyError):
+            pass
+
+        charge_data = {
+                    'charge_name': 'eBay List',
+                    'charge_price': 4.99
+                }
+
+        create_charge = CreateChargeForm(request.user, data=charge_data)
+        if create_charge.is_valid():
+            charge_resp = create_charge.save()
+            return HttpResponseRedirect(charge_resp['recurring_application_charge']['confirmation_url'])
